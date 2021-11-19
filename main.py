e2bd1a1ba8f10d66988873f39dab84236d33ea9b
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2021-11-19 12:21:56 +0100

    Load in sequences from .aln file and pass to fast_tree algo

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
 
 Based on the FastTree algorithm as introduced by Price et al. [1]
-[1] Price at al. FastTree: Computing Large Minimum Evolution Trees with Profiles instead of a Distance Matrix. Molecular Biology and Evolution, vol 26 (7). 2009.
+[1] Price at al. FastTree: Computing Large Minimum Evolution Trees with Profiles instead of a Distance Matrix.
+    Molecular Biology and Evolution, vol 26 (7). 2009.
 
 
 Authors:
@@ -13,11 +14,14 @@ import sys
 import os
 import argparse
 
+from src.fast_tree import fast_tree
+
+
 # This is the Project Root, important that run.py is inside this folder for accurate ROOT_DIR
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Argument Parser
-PARSER = argparse.ArgumentParser(prog='main.py', 
+PARSER = argparse.ArgumentParser(prog='main.py',
                                  description="Runner of Group 4's implementation of the FastTree algorithm")
 
 PARSER.add_argument('--file', '-f',
@@ -30,28 +34,67 @@ PARSER.add_argument('--experiment', '-e',
                     default="experiment")
 
 def main() -> None:
-    """
-    The start of the program. This function processes user input and calls the FastTree algorithm.
+    """Program entry function.
 
-    @return: None
+    This function processes user input and calls the FastTree algorithm.
+
+    Returns:
+        (None)
     """
-    # Print all the user entered arguments in a neatly organized fashion
+    # Get arguments from parser
     args = PARSER.parse_args()
 
+    # Check for input file in args
     if args.aln_file is None:
         print("No input .aln file specified. Use -f '/path/to/sequences.aln' to insert one. Exiting program.")
         sys.exit()
 
-    with open(args.aln_file, 'r') as file:
-        try:
-            data = file.read().split('\n')
-        except yaml.YAMLError as exc:
-            print("Incorrect .aln file detected!")
-            print(exc)
-    
+    # Fetch sequences from input file
+    sequences = readinput(args.aln_file)
+
+    # Run FastTree
+    fast_tree(sequences)
+
+
+def readinput(filepath):
+    """Processes .aln file into a mapping.
+
+    Reads in a file whose path was provided in the program's arguments.
+    Assumes the input is .aln formatted, then loops through the file
+    and creates mappings of the sequences to their names.
+
+    Args:
+        filepath (str): String containg the path of the input file
+
+    Returns:
+        (dict): A mapping of sequences to their names, as found in the input file
+    """
+    # Check if .aln format
+    if not filepath.endswith('.aln'):
+        print("Input file should be .aln formatted!")
+        sys.exit()
+
+    # Try reading in the file
+    try:
+        file = open(filepath, 'r')
+    except IOError as exception:
+        print("Input file could not be opened : ")
+        print(exception)
+        sys.exit()
+
+    # File has successfully been loaded in. Now process data from it
+    with open(filepath, 'r') as file:
+        data = file.read().splitlines()  # Read in file, get rid of '\n' characters
+
     # Process the .aln file into a dictionary mapping {name -> sequence}
     sequences = {}
-    
+    for i in range(0, len(data) - 1, 2):  # Since file is in format (name, sequence pairs), move in steps of 2
+        name = data[i][1:]  # [1:] to get rid of '>' character
+        sequence = data[i + 1]
+
+        sequences[name] = sequence
+
+    return sequences
 
 if __name__ == "__main__":
-   main()
+    main()
---
+++ src/fast_tree.py
@@ -0,0 +1,17 @@
+"""
+Maybe some comments about implementation here.
+"""
+import pprint  # For pretty printing (Replace with own code before submission)
+
+def fast_tree(sequences) -> str:
+    """FastTree Algorithm.
+
+    Args:
+        sequences (dict): Mapping of sequences to their names, as was provided in the program's input
+
+    Returns:
+        (str): A phylogenetic tree in Newick format.
+    """
+
+    print("The sequences entered into the program : ")
+    pprint.pprint(sequences)
