ff983703c5cbe325499cab60b908c79f00a1abc5
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2021-11-26 14:24:29 +0100

    Add r() and create profile function for a single node

--- src/fast_tree.py
+++ src/fast_tree.py
@@ -8,6 +8,9 @@ References to page numbers in this code are referring to the paper:
 """
 import math
 import pprint as pp # For pretty printing (Replace with own code before submission)
+import sys
+
+from src.node import Node
 
 def fast_tree(sequences) -> str:
     """FastTree Algorithm.
@@ -20,34 +23,31 @@ def fast_tree(sequences) -> str:
     """
     print("The sequences entered into the program : ")
     pp.pprint(sequences)
+    nodes = []
+    for seq in sequences.keys():
+        node = Node(seq, sequences[seq])
+        nodes.append(node)
 
     # Actual first step : Unique sequences ( page 1646, do later )
 
     # Step 1 of algorithm : Create total profile T
-    all_sequences = list(sequences.values())
-    T = Profile(all_sequences)
+    T = Profile(nodes)
+    print("Total profile T")
     pp.pprint(T)
 
     # Step 2 : Top hits sequence
     # Skip for now
 
     # Step 3 : Create initial topology
-
-    # calculate Profile for internal nodes
-    Hypothetical_internal_node = [sequences['2'] , sequences['4'], sequences['5']]
-    Node_profile = Profile(Hypothetical_internal_node)
-    Sequence_distance = uncorrectedDistance(Node_profile)
-
-    combi = makeCombisofChildren(Hypothetical_internal_node)
-    Sequence_distance2 = SequenceDistance(combi, len(sequences['2']))  #calculates list of all combi's of children
-    # print('Profile between 2 and 4 is: ' + str(Node_profile))
-    # print('Their sequence distance (uncorrected distance) is: ' + str(Sequence_distance))
-    # print('Their sequence distance (uncorrected distance) is: ' + str(Sequence_distance2)) 
+    minimized_join = minimize_nj_criterion(nodes)
 
 
 # Calculate Profile of internal nodes
-def Profile(internalNodes):
-    columns = [''.join(seq) for seq in zip(*internalNodes)]
+def Profile(nodes):
+    sequences = []
+    for node in nodes:
+        sequences.append(node.sequence)
+    columns = [''.join(seq) for seq in zip(*sequences)]
     return [[float(col.count(base)) / float(len(col)) for base in 'ACGT'] for col in columns]
 
 def uncorrectedDistance(profile): #i.e. the fraction of positions that differ aka #difference/sequence length by using profiles
@@ -65,7 +65,6 @@ def makeCombisofChildren(children): #make combinations to calculate the hamming 
         for j in range(i+1, len(children)):
             combi.append( [v1, children[j]])
     return combi
-
 
 def HammingDistance(combi): #calculate hamming distance between combinations of children
     distance = []
@@ -85,8 +84,55 @@ def SequenceDistance(combi, k): #ratio of #difference/sequence length by using h
     return seqDis
 
 def out_distance(i, nodes):
+    """Calculates r distance of i : r(i)
+
+    Args:
+        i (Node) : 
     """
+    active_nodes = 1  # i is always an active node
+    dist_to_others = 0
+    for j in nodes:
+        if j.name == i:
+            continue
+        if not j.active:
+            continue
+
+        active_nodes += 1
+
+        profile_i_j = Profile([i, j])
+
+        dist_to_others += uncorrectedDistance(profile_i_j)
+
+    # Don't divide by 0
+    if active_nodes == 2:
+        return dist_to_others
+
+    r = dist_to_others / (active_nodes - 2)
+    # print("Out distance r({}) = ".format(i.name), r)
+    return r
+
+def minimize_nj_criterion(nodes):
+    """Returns i,j for which d(i, j) - r(i) -r(j) is minimal
     """
+    active_nodes = []
+    for node in nodes:
+        if node.active:
+            active_nodes.append(node)
+
+    min_dist = sys.float_info.max
+    best_join = (0, 0)
+    for i  in active_nodes:
+        for j in active_nodes:
+            if i == j:
+                continue
+            criterion = uncorrectedDistance(Profile([i, j])) - out_distance(i, nodes) - out_distance(j, nodes)
+            if criterion < min_dist:
+                min_dist = criterion
+                best_join = (i, j)
+    
+    print("Minimized distance = ", min_dist, "of nodes ", best_join[0].name, best_join[1].name)
+
+    return best_join
 
 def JC_distance(d_u: float) -> float:
     """Compute Jukes-Cantor distance of FastTree's uncorrected distance
--- src/node.py
+++ src/node.py
@@ -1,3 +1,6 @@
+from fast_tree import Profile
+
+
 class Node:
 
     def __init__(self, name, sequence):
@@ -7,6 +10,17 @@ class Node:
         self.leftchild = None
         self.rightchild = None
         self.active = True
+        self.profile = self.create_profile(sequence)
+
+    def create_profile(self, sequence):
+        profile = []
+        nt_values = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+
+        for nt in sequence:
+            column = [0 for i in range(4)]
+            column[nt_values[nt]] = 1.0
+            profile.append(column)
+        return profile
 
     def set_inactive(self):
         self.active = False
